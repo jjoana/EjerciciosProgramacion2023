0edd0c1addc352bfbfa933de0c42e91eb73e6593
Jesus Joana <72490467+jjoana@users.noreply.github.com>
2023-02-18 12:07:49 +0000

    Ejercicios de programacion

--- src/Ejercicio2_2.java
+++ src/Ejercicio2_2.java
@@ -1,4 +1,4 @@
-public class App{
+public class Ejercicio2_2{
 
     public static void main(String[] args){
         Factorial factorial = new Factorial();
---
+++ src/Ejercicio2_3.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Ejercicio2_3 {
+    public static void main(String[] args){
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+    }
+
+class ListaNumeros{
+    public int FactorialRecursivo(int n){
+        int valor =0;
+        if(!)
+        }   
+    }
+}
