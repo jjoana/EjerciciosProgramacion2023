0c7f63997c3379152fda6b6893a4a19d608ccc46
Jesus Joana <72490467+jjoana@users.noreply.github.com>
2023-02-18 11:13:15 +0000

    Ejercicio 2.1

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App{
+
+    public static void main(String[] args){
+        Factorial factorial = new Factorial();
+        System.out.println("El valor es: " + factorial.factorialIterativo(5));
+        System.out.println("El valor es: " + factorial.factorialRecursivo(5));
+    }
+}
+
+class Factorial{
+    public int factorialIterativo(int n){
+        int valor = 1;
+        for (int i=n; i>1; i--){
+          valor*=i;
+        }
+        return valor;
+    }
+    
+    public int factorialRecursivo(int n){
+        if(n>1){
+            n*= factorialRecursivo(n-1);
+        }
+        return n;
+    }
+}
