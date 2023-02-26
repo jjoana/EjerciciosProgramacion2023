8a6aa5871e8928885774d3905bc1ffc9d2bc1b7f
Jesus Joana <72490467+jjoana@users.noreply.github.com>
2023-02-26 12:38:06 +0000

    Metodo Iterativo realizado en el Ejercicio 2_8

--- src/Ejercicio2_8.java
+++ src/Ejercicio2_8.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class Ejercicio2_8 {
-    public static void main(String[] args){
-        ArrayList<Integer> lista =  new ArrayList<Integer>();
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<Integer>();
         lista.add(1);
         lista.add(2);
         lista.add(3);
@@ -10,23 +12,33 @@ public class Ejercicio2_8 {
 
         calculoListaPares objeto = new calculoListaPares(lista);
         System.out.println(objeto.ListaParesIterativo());
+        System.out.println(objeto.SumaListaPares(lista));
     }
 }
 
-class calculoListaPares{
-    ArrayList<Integer> listaInicial; 
+class calculoListaPares {
+    ArrayList<Integer> listaInicial;
     ArrayList<Integer> listaPares = new ArrayList<Integer>();
 
     public calculoListaPares(ArrayList<Integer> lista) {
         listaInicial = lista;
     }
 
-    public ArrayList<Integer> ListaParesIterativo(){
-        for(int i=1; i<= listaInicial.size(); i++){
-            if(i % 2 == 0){
-                listaPares.add(listaInicial.get(i-1));
+    public ArrayList<Integer> ListaParesIterativo() {
+        for (int i = 1; i <= listaInicial.size(); i++) {
+            if (i % 2 == 0) {
+                listaPares.add(listaInicial.get(i - 1));
             }
         }
-        return(listaPares);
+        return listaPares;
+    }
+
+    public int SumaListaPares(ArrayList<Integer> milista) {
+        int resultado = 0;
+        listaPares.size();
+        for (int i = listaPares.size() - 1; i >= 0; i--) {
+            resultado += listaPares.get(i);
+        }
+        return resultado;
     }
 }
