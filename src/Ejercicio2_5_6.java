0f58577aeec87f14b64dd171a2c141ab0ff692ee
Jesus Joana <72490467+jjoana@users.noreply.github.com>
2023-02-26 10:55:51 +0000

    MediaAritmetica finalizada de forma recursiva e iterativa

--- src/Ejercicio2_5_6.java
+++ src/Ejercicio2_5_6.java
@@ -2,34 +2,43 @@ import java.util.ArrayList;
 
 public class Ejercicio2_5_6 {
     public static void main (String[] args){
-        ArrayList<Integer> lista = new ArrayList<Integer>();
+        ArrayList<Float> lista = new ArrayList<Float>();
         int tamano;
-        lista.add(1);
-        lista.add(2);
-        lista.add(3);
-        lista.add(4);
-        lista.add(5);
+        lista.add(3f);
+        lista.add(2f);
+        lista.add(6f);
         tamano=lista.size();
 
         Calculo objeto = new Calculo(lista);
-        System.out.println(objeto.mediaAitmeticaRecursivo(tamano));
+        System.out.println(objeto.mediaAritmeticaRecursivo(tamano));
+        System.out.println(objeto.mediaAritmeticaIterativa(lista,tamano));
     }
 }
 
 class Calculo{
+    int tamanoLista;
+    ArrayList<Float> listaI;
 
-    ArrayList<Integer> listaii = new ArrayList<Integer>();
-    
-    public Calculo(ArrayList<Integer> listaTrabajo){
-        listaii = listaTrabajo;
+    public Calculo(ArrayList<Float> miLista){
+        listaI = (ArrayList<Float>)miLista.clone();
+        tamanoLista=miLista.size();
     }
 
-    public float mediaAitmeticaRecursivo(int tamano){
-        float valor = listaii.get(0)/tamano;
-        listaii.remove(0);
+    public float mediaAritmeticaRecursivo(int tamano){
+        float valor = listaI.get(0)/tamanoLista;
+        listaI.remove(0);
         if(tamano>1){
-            valor= mediaAitmeticaRecursivo(tamano-1);
+            valor+=mediaAritmeticaRecursivo(tamano-1);
         }
         return valor;
+    }
+
+    public float mediaAritmeticaIterativa(ArrayList<Float>miLista, int tamano){
+        float valor= 0f;
+        for (int i = miLista.size() - 1; i >= 0; i--) {
+            valor += miLista.get(i)/tamanoLista;
+        }
+        return valor;
+
     }
 }
