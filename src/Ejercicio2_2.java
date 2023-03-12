3281e555d5bb88a3df481748cd1c83f5891a78fe
Jesus Joana <72490467+jjoana@users.noreply.github.com>
2023-03-12 10:36:54 +0000

    Creada la excepcion de numero negativo en el ejercicio 2.2

--- src/Ejercicio2_2.java
+++ src/Ejercicio2_2.java
@@ -1,26 +1,54 @@
-public class Ejercicio2_2{
+import java.util.Scanner;
 
-    public static void main(String[] args){
+public class Ejercicio2_2 {
+
+    public static void main(String[] args) {
         Factorial factorial = new Factorial();
-        System.out.println("El valor es: " + factorial.factorialIterativo(10));
-        System.out.println("El valor es: " + factorial.factorialRecursivo(10));
+        try {
+            Scanner sc = new Scanner(System.in);
+            int valor;
+            int resultado;
+            
+            System.out.println("Introduzca el valor");
+            valor = sc.nextInt();
+
+            resultado = factorial.factorialRecursivo(valor);
+            System.out.println(resultado);
+
+            resultado = factorial.factorialIterativo(valor);
+            System.out.println(resultado);
+
+        } catch (NegativeNumberException e) {
+            System.out.println("El numero que ha introducido es negativo");
+        }
     }
 }
-class Factorial{
-    public int factorialIterativo(int n){
+
+class Factorial {
+    public int factorialIterativo(int n) throws NegativeNumberException {
+        if (n < 0) {
+            throw (new NegativeNumberException());
+        }
         int valor = 1;
-        for (int i=n; i>1; i--){
-          valor*=i;
+        for (int i = n; i > 1; i--) {
+            valor *= i;
         }
         return valor;
     }
-    
-    public int factorialRecursivo(int n){
-        if(n>0){
-            n*= factorialRecursivo(n-1);
-        }else {
+
+    public int factorialRecursivo(int n) throws NegativeNumberException {
+        if (n < 0) {
+            throw (new NegativeNumberException());
+        }
+        if (n > 0) {
+            n *= factorialRecursivo(n - 1);
+        } else {
             return 1;
         }
         return n;
     }
+}
+
+class NegativeNumberException extends Exception {
+
 }
