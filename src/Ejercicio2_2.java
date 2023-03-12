ea872e56f1fe87eda3618248875971a8b8e489ce
Jesus Joana <72490467+jjoana@users.noreply.github.com>
2023-03-12 11:18:04 +0000

    Modificaciones de excepciones en el ejercicio 2_2

--- src/Ejercicio2_2.java
+++ src/Ejercicio2_2.java
@@ -4,42 +4,52 @@ public class Ejercicio2_2 {
 
     public static void main(String[] args) {
         Factorial factorial = new Factorial();
-        try {
-            Scanner sc = new Scanner(System.in);
-            int valor;
-            int resultado;
-            
-            System.out.println("Introduzca el valor");
-            valor = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        long valor;
+        long resultado;
+        
+        System.out.println("Introduzca el valor");
+        valor = sc.nextLong();
 
+        try {
             resultado = factorial.factorialRecursivo(valor);
             System.out.println(resultado);
-
             resultado = factorial.factorialIterativo(valor);
             System.out.println(resultado);
-
         } catch (NegativeNumberException e) {
             System.out.println("El numero que ha introducido es negativo");
+        } catch (ToLargeNumberException e){
+            System.out.println("El numero es demasiado grande");
         }
     }
 }
 
 class Factorial {
-    public int factorialIterativo(int n) throws NegativeNumberException {
+    public long factorialIterativo(long n) throws NegativeNumberException, ToLargeNumberException {
         if (n < 0) {
             throw (new NegativeNumberException());
         }
-        int valor = 1;
-        for (int i = n; i > 1; i--) {
+
+        if(n>=21){
+            throw (new ToLargeNumberException());
+        }
+        
+        long valor = 1;
+        for (long i = n; i > 1; i--) {
             valor *= i;
         }
         return valor;
     }
 
-    public int factorialRecursivo(int n) throws NegativeNumberException {
+    public long factorialRecursivo(long n) throws NegativeNumberException, ToLargeNumberException {
         if (n < 0) {
             throw (new NegativeNumberException());
         }
+
+        if(n>=21){
+            throw (new ToLargeNumberException());
+        }
+
         if (n > 0) {
             n *= factorialRecursivo(n - 1);
         } else {
@@ -51,4 +61,8 @@ class Factorial {
 
 class NegativeNumberException extends Exception {
 
+}
+
+
+class ToLargeNumberException extends Exception{
 }
